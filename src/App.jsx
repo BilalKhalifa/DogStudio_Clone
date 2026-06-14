68794977ca4f606af8c24eeb92c3a02eb4fe0f85
BilalKhalifa <bilalkhalifa9988@gmail.com>
2026-06-14 19:08:41 +0530

    feat: initialize 3D website project with React Three Fiber, custom Dog model, and material assets

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,24 @@ import { Canvas } from '@react-three/fiber'
 const App = () => {
   return (
     <>
-      <Canvas>
-        <Dog />
-      </Canvas>
+      <main>
+        <Canvas style={{
+          height: '100vh',
+          width: '100vw',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          zIndex: 1,
+          backgroundImage: "url(/background-xl.png)",
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover'
+        }}>
+          <Dog />
+        </Canvas>
+        <section id= 'section1'></section>
+        <section id= 'section2'></section>
+        <section id= 'section3'></section>
+      </main>
     </>
   )
 }
--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,16 +1,65 @@
-import { OrbitControls, useGLTF } from '@react-three/drei'
+import { OrbitControls, useAnimations, useTexture } from '@react-three/drei'
+import * as THREE from "three"
+import { useGLTF } from '@react-three/drei'
 import { Canvas, useThree } from '@react-three/fiber'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { normalMap } from 'three/tsl'
 
 
 const Dog = () => {
 
-    const scene = useGLTF('/models/dog.drc.glb')
+    const model = useGLTF('/models/dog.drc.glb')
+
+    useThree(({camera,scene,gl})=>{
+        camera.position.z = 1
+        gl.toneMapping = THREE.ReinhardToneMapping,
+        gl.outputColorSpace = THREE.SRGBColorSpace
+    })
+
+    const { actions } = useAnimations(model.animations, model.scene)
+
+    useEffect(()=>{
+        actions['Take 001'].play()
+    }, [actions])
+
+    const textures = useTexture({
+
+        normalMap:'/dog_normals.jpg',
+        branchNormalMap: '/branches_normals.jpg',
+        sampleMatCap: '/matcap/mat-2.png',
+        branchMatCap: '/branches_diffuse.jpg'
+    },(loadedTexture)=>{
+        Object.values(loadedTexture).forEach((texture)=>{
+            texture.flipY = false;
+            texture.colorSpace = THREE.SRGBColorSpace;
+        });
+    })
+
+    const dogMaterial  = new THREE.MeshMatcapMaterial({
+        normalMap:textures.normalMap,
+        matcap: textures.sampleMatCap
+    })
+    
+    const branchesMaterial = new THREE.MeshMatcapMaterial({
+        normalMap: textures.branchNormalMap,
+        matcap: textures.branchMatCap
+    })
+
+    model.scene.traverse((child)=>{
+        if(child.name.includes("DOG")){
+            child.material = dogMaterial
+        }
+
+        if(child.name.includes("BRANCHS")){
+            child.material = branchesMaterial
+        }
+    })
+
 
     return (
         <>
-            <primitive object={scene}/>
-            <OrbitControls />
+            <primitive object={model.scene} position={[0.5,-1.2,0]} scale={[2,2,2]} rotation={[0,Math.PI / 4.6 ,0]}/>
+            <directionalLight intensity={10} color={0xFFFFFF} position={[0,5,5]} />
         </>
     )
 }
